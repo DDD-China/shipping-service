204413036a99d2f0c677e789597d8d3c0d23ae6f
linwenjun <linwenjun@163.com>
2018-12-20 00:02:56 +0800

    change script

--- ci/genImage.sh
+++ ci/genImage.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
+IMAGE_REGISTRY=955065381857.dkr.ecr.cn-north-1.amazonaws.com.cn
 IMAGE_REPO=shipping-service
 
 $(aws ecr get-login --no-include-email --region cn-north-1)
 
-
-docker build -t $IMAGE_REPO:$BUILD_NUMBER .
-docker tag $IMAGE_REPO:$BUILD_NUMBER $IMAGE_REPO:latest
-docker push 955065381857.dkr.ecr.cn-north-1.amazonaws.com.cn/$IMAGE_REPO:latest
-docker push 955065381857.dkr.ecr.cn-north-1.amazonaws.com.cn/$IMAGE_REPO:$BUILD_NUMBER
+docker build -t $IMAGE_REGISTRY/$IMAGE_REPO:$BUILD_NUMBER .
+docker tag $IMAGE_REGISTRY/$IMAGE_REPO:$BUILD_NUMBER $IMAGE_REGISTRY/$IMAGE_REPO:latest
+docker push $IMAGE_REGISTRY/$IMAGE_REPO:latest
+docker push $IMAGE_REGISTRY/$IMAGE_REPO:$BUILD_NUMBER
