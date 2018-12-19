ad01d8a26bc873049e293202ea229688763dd407
linwenjun <linwenjun@163.com>
2018-12-19 23:51:51 +0800

    change script

--- ci/genImage.sh
+++ ci/genImage.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/env bash
 
+$(aws ecr get-login --no-include-email --region cn-north-1)
+
+docker build -t shipping-service .
+
+docker push 955065381857.dkr.ecr.cn-north-1.amazonaws.com.cn/shipping-service:latest
