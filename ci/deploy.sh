4ac47158e790607e5bf6a32ea34fce96536f2129
linwenjun <linwenjun@163.com>
2018-12-19 23:59:03 +0800

    change script

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,11 +1,3 @@
 #!/usr/bin/env bash
 
-IMAGE_REPO=shipping-service
-
-$(aws ecr get-login --no-include-email --region cn-north-1)
-
-
-docker build -t $IMAGE_REPO:$BUILD_NUMBER .
-docker tag $IMAGE_REPO:$BUILD_NUMBER $IMAGE_REPO:latest
-docker push 955065381857.dkr.ecr.cn-north-1.amazonaws.com.cn/$IMAGE_REPO:latest
-docker push 955065381857.dkr.ecr.cn-north-1.amazonaws.com.cn/$IMAGE_REPO:$BUILD_NUMBER
+echo "Deploy"
--- ci/genImage.sh
+++ ci/genImage.sh
@@ -6,7 +6,6 @@ $(aws ecr get-login --no-include-email --region cn-north-1)
 
 
 docker build -t $IMAGE_REPO:$BUILD_NUMBER .
-
 docker tag $IMAGE_REPO:$BUILD_NUMBER $IMAGE_REPO:latest
 docker push 955065381857.dkr.ecr.cn-north-1.amazonaws.com.cn/$IMAGE_REPO:latest
 docker push 955065381857.dkr.ecr.cn-north-1.amazonaws.com.cn/$IMAGE_REPO:$BUILD_NUMBER
