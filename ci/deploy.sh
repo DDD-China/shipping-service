2cc626cbc0f22f2c851b5eea2d6e14fa3bee539b
linwenjun <linwenjun@163.com>
2018-12-20 00:51:54 +0800

    change deploy script

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -3,5 +3,5 @@
 IMAGE_REGISTRY=955065381857.dkr.ecr.cn-north-1.amazonaws.com.cn
 IMAGE_REPO=shipping-service
 IMAGE_URI=$IMAGE_REGISTRY/$IMAGE_REPO:$BUILD_NUMBER
-
+sed "s@IMAGE_URI@$IMAGE_URI@g" ci/kube-deploy.yaml
 sed "s@IMAGE_URI@$IMAGE_URI@g" ci/kube-deploy.yaml | sudo kubectl --insecure-skip-tls-verify --kubeconfig $KUBE_CONFIG_FILE apply -f -
