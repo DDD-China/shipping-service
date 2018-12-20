2cae800a72d96a937a69b6b8278cf328c5d1a0c0
linwenjun <linwenjun@163.com>
2018-12-20 08:33:27 +0800

    fix docker build command issue

--- ci/genImage.sh
+++ ci/genImage.sh
@@ -6,7 +6,7 @@ IMAGE_REPO=shipping-service
 $(aws ecr get-login --no-include-email --region cn-north-1)
 
 ./gradlew docker
-docker tag com.dmall/$IMAGE_REPO:latest $IMAGE_REGISTRY/$IMAGE_REPO:$BUILD_NUMBER .
+docker tag com.dmall/$IMAGE_REPO:latest $IMAGE_REGISTRY/$IMAGE_REPO:$BUILD_NUMBER
 docker tag com.dmall/$IMAGE_REPO:latest $IMAGE_REGISTRY/$IMAGE_REPO:latest
 docker push $IMAGE_REGISTRY/$IMAGE_REPO:latest
 docker push $IMAGE_REGISTRY/$IMAGE_REPO:$BUILD_NUMBER
